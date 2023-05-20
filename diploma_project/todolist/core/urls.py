316e557c51066bd1f02f90ba4943685602ce3b7b
Wokelen <99183991+Wokelen@users.noreply.github.com>
2023-05-21 00:26:01 +0300

    Update urls.py

--- diploma_project/todolist/core/urls.py
+++ diploma_project/todolist/core/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('signup', views.RegistrationView.as_view(), name='signup'),
+    path('signup', views.UserCreateView.as_view(), name='signup'),
     path('login', views.LoginView.as_view(), name='login'),
     path('profile', views.ProfileView.as_view(), name='profile'),
-    path('update_password', views.UpdatePasswordView.as_view(), name='update_password'),
+    path("update_password", views.PasswordUpdateView.as_view(), name='update_password'),
 ]
